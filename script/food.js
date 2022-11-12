97c48d73fd8947ae4496f94201bb27555def6c93
Bhupender Soni <official.soni532@gmail.com>
2022-11-13 02:30:52 +0530

    minior changes

--- script/food.js
+++ script/food.js
@@ -1,15 +1,29 @@
-let foodData = JSON.parse(localStorage.getItem('foodData')) || [];
+// let foodData = JSON.parse(localStorage.getItem('foodData')) || [];
 let breakfast = document.querySelector('#breakfast')
 let lunch = document.querySelector('#lunch')
 let dinner = document.querySelector('#dinner')
 let snacks = document.querySelector('#snacks')
-let categoryArr = [breakfast,lunch,dinner,snacks]
-function handleUpdate(){
+let categoryArr = [breakfast, lunch, dinner, snacks]
+
+function handleUpdate() {
     categoryArr.forEach(el => {
-        let data = getData
+        let data = getdata(dynamicUrl(el.value))
+        data.then(data => console.log(data))
     })
 }
 
-function getData(){
-
+function dynamicUrl(val) {
+    return `https://api.spoonacular.com/recipes/guessNutrition?title=${val}&apiKey=6fb8b413cfcf4fd9aae88cb0f4b89dcb`
 }
+
+async function getdata(url) {
+    try {
+        let res = await fetch(url)
+        let data = await res.json()
+        return data
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
